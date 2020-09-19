4f7249618858be62aaf50984e977ecdad05a0741
Abishek V Ashok <abishekvashok@gmail.com>
2020-09-19 08:35:41 +0530

    Initial Commit

---
+++ HeartAcuity/HeartAcuity.h
@@ -0,0 +1,19 @@
+//
+//  HeartAcuity.h
+//  HeartAcuity
+//
+//  Created by Abishek on 19/09/20.
+//  Copyright © 2020 HeartAcuityTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeartAcuity.
+FOUNDATION_EXPORT double HeartAcuityVersionNumber;
+
+//! Project version string for HeartAcuity.
+FOUNDATION_EXPORT const unsigned char HeartAcuityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeartAcuity/PublicHeader.h>
+
+
