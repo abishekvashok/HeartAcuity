10acbcccf9e7fb42c8469c85c75d98df70bdb271
Abishek V Ashok <abishekvashok@gmail.com>
2020-09-19 14:45:12 +0530

    fixes black screen

---
+++ HeartAcuity/HeartAcuity/LearnMoreViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LearnMoreViewController.swift
+//  HeartAcuity
+//
+//  Created by Abishek on 19/09/20.
+//  Copyright © 2020 HeartAcuityTeam. All rights reserved.
+//
+
+import UIKit
+
+class LearnMoreViewController: UIViewController {
+
+}
