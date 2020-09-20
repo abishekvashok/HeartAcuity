c8fdae4ab0b622991153caefc74a29bc69446cfb
Abishek V Ashok <abishekvashok@gmail.com>
2020-09-20 16:03:53 +0530

    fixes code

--- HeartAcuity/HeartAcuity/ScoreCalculatorViewController.swift
+++ HeartAcuity/HeartAcuity/ScoreCalculatorViewController.swift
@@ -23,12 +23,29 @@ class ScoreCalculatorViewController: UIViewController {
     
     @IBOutlet weak var calculateRisk: UIButton!
     
+    @IBOutlet weak var rrContainer: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ageEntry.text = String(UserDefaults.standard.double(forKey: "age"))
         heightEntry.text = String(UserDefaults.standard.double(forKey: "height"))
         weightEntry.text = String(UserDefaults.standard.double(forKey: "weight"))
+    }
+    
+    @IBAction func CalculateRisk(_ sender: Any) {
+    
+        let model = HA_xgb_test_coreml()
+        
+        guard let modelOutput = try? model.prediction(policyID: 1.1, eq_site_limit: 1.1, hu_site_limit: 1.1, fl_site_limit: 1.1, fr_site_limit: 1.1, tiv_2011: 1.1, tiv_2012: 1.1, eq_site_deductible: 1.1, hu_site_deductible: 1.1, fl_site_deductible: 1.1, fr_site_deductible: 1.1, point_latitude: 1.1, point_longitude: 1.1, point_granularity: 1.1, statecode_ohe_0: 1.1, county_ohe_0: 1.1, county_ohe_1: 1.1, county_ohe_2: 1.1, county_ohe_3: 1.1, county_ohe_4: 1.1, county_ohe_5: 1.1, county_ohe_6: 1.1, county_ohe_7: 1.1, county_ohe_8: 1.1, county_ohe_9: 1.1, county_ohe_10: 1.1, county_ohe_11: 1.1, county_ohe_12: 1.1, county_ohe_13: 1.1, county_ohe_14: 1.1, county_ohe_15: 1.1, county_ohe_16: 1.1, county_ohe_17: 1.1, county_ohe_18: 1.1, county_ohe_19: 1.1, county_ohe_20: 1.1, county_ohe_21: 1.1, county_ohe_22: 1.1, county_ohe_23: 1.1, county_ohe_24: 1.1, county_ohe_25: 1.1, county_ohe_26: 1.1, county_ohe_27: 1.1, county_ohe_28: 1.1, county_ohe_29: 1.1, county_ohe_30: 1.1, county_ohe_31: 1.1, county_ohe_32: 1.1, county_ohe_33: 1.1, county_ohe_34: 1.1, county_ohe_35: 1.1, county_ohe_36: 1.1, county_ohe_37: 1.1, county_ohe_38: 1.1, county_ohe_39: 1.1, county_ohe_40: 1.1, county_ohe_41: 1.1, county_ohe_42: 1.1, county_ohe_43: 1.1, county_ohe_44: 1.1, county_ohe_45: 1.1, county_ohe_46: 1.1, county_ohe_47: 1.1, county_ohe_48: 1.1, county_ohe_49: 1.1, county_ohe_50: 1.1, county_ohe_51: 1.1, county_ohe_52: 1.1, county_ohe_53: 1.1, county_ohe_54: 1.1, county_ohe_55: 1.1, county_ohe_56: 1.1, county_ohe_57: 1.1, county_ohe_58: 1.1, county_ohe_59: 1.1, county_ohe_60: 1.1, county_ohe_61: 1.1, county_ohe_62: 1.1, county_ohe_63: 1.1, county_ohe_64: 1.1, county_ohe_65: 1.1, county_ohe_66: 1.1, line_ohe_0: 1.1, line_ohe_1: 1.1, construction_ohe_0: 1.1, construction_ohe_1: 1.1, construction_ohe_2: 1.1, construction_ohe_3: 1.1)
+            else{
+                fatalError("Unexpected runtime error.")
+            }
+        
+        UserDefaults.standard.set(Double(modelOutput.classProbability[0]!), forKey: "strokeScore")
+    }
+    
+    
+    @IBAction func calculateRRInteveral(_ sender: Any) {
         if HKHealthStore.isHealthDataAvailable() {
             let healthStore = HKHealthStore()
             let allTypes = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!])
@@ -38,13 +55,23 @@ class ScoreCalculatorViewController: UIViewController {
                     // Create the electrocardiogram sample type.
                     if #available(iOS 14.0, *) {
                         let ecgType = HKObjectType.electrocardiogramType()
+                        var armmsd = 0
+                        var l = 0
                         let ecgQuery = HKSampleQuery(sampleType: ecgType,
                                                      predicate: nil,
                                                      limit: HKObjectQueryNoLimit,
                                                      sortDescriptors: nil) { (query, samples, error) in
+                            l = l + 1
                             if let error = error {
                                 // Handle the error here.
                                 fatalError("*** An error occurred \(error.localizedDescription) ***")
+                            }
+                            if let done = done {
+                                if(l != 0){
+                                    armmsd = armmsd / l
+                                    rrContainer.text = String(armmsd)
+                                }
+                                
                             }
                             
                             guard let ecgSamples = samples as? [HKElectrocardiogram] else {
@@ -52,23 +79,44 @@ class ScoreCalculatorViewController: UIViewController {
                             }
                             
                             for sample in ecgSamples {
+                                i = 0
+                                j = 0
+                                startI = 0
+                                k = 0
+                                var avg = 0
+                                var rmssd = 0
                                 let voltageQuery = HKElectrocardiogramQuery(sample) { (query, result) in
                                     switch(result) {
                                     
                                     case .measurement(let measurement):
                                         if let voltageQuantity = measurement.quantity(for: .appleWatchSimilarToLeadI) {
-                                            // Do something with the voltage quantity here.
-
+                                            i = i + 1
+                                            if(voltageQuantity > 1 && j = 0){
+                                                if(i == 1){
+                                                    startI = i
+                                                } else {
+                                                    var diff = i - startI
+                                                    diff = diff / (ecgSamples.SampleFrequency())
+                                                    diff = diff * diff
+                                                    k = k + 1
+                                                    avg = avg + diff
+                                                }
+                                                j = 1
+                                            } else {
+                                                j = 0
+                                            }
                                         }
 
                                     case .error(let error):
                                         // Handle the error here.
                                         print("Error")
+                                        rmssd = avg/k
+                                        armmsd + rmssd
                                     case .done:
-                                        print("Error")
+                                        print("Done")
                                     }
-                                    
                                 }
+
 
                                 // Execute the query.
                                 healthStore.execute(voltageQuery)
@@ -85,18 +133,4 @@ class ScoreCalculatorViewController: UIViewController {
             }
         }
     }
-    
-    @IBAction func CalculateRisk(_ sender: Any) {
-    
-        let model = HA_xgb_test_coreml()
-        
-        guard let modelOutput = try? model.prediction(policyID: 1.1, eq_site_limit: 1.1, hu_site_limit: 1.1, fl_site_limit: 1.1, fr_site_limit: 1.1, tiv_2011: 1.1, tiv_2012: 1.1, eq_site_deductible: 1.1, hu_site_deductible: 1.1, fl_site_deductible: 1.1, fr_site_deductible: 1.1, point_latitude: 1.1, point_longitude: 1.1, point_granularity: 1.1, statecode_ohe_0: 1.1, county_ohe_0: 1.1, county_ohe_1: 1.1, county_ohe_2: 1.1, county_ohe_3: 1.1, county_ohe_4: 1.1, county_ohe_5: 1.1, county_ohe_6: 1.1, county_ohe_7: 1.1, county_ohe_8: 1.1, county_ohe_9: 1.1, county_ohe_10: 1.1, county_ohe_11: 1.1, county_ohe_12: 1.1, county_ohe_13: 1.1, county_ohe_14: 1.1, county_ohe_15: 1.1, county_ohe_16: 1.1, county_ohe_17: 1.1, county_ohe_18: 1.1, county_ohe_19: 1.1, county_ohe_20: 1.1, county_ohe_21: 1.1, county_ohe_22: 1.1, county_ohe_23: 1.1, county_ohe_24: 1.1, county_ohe_25: 1.1, county_ohe_26: 1.1, county_ohe_27: 1.1, county_ohe_28: 1.1, county_ohe_29: 1.1, county_ohe_30: 1.1, county_ohe_31: 1.1, county_ohe_32: 1.1, county_ohe_33: 1.1, county_ohe_34: 1.1, county_ohe_35: 1.1, county_ohe_36: 1.1, county_ohe_37: 1.1, county_ohe_38: 1.1, county_ohe_39: 1.1, county_ohe_40: 1.1, county_ohe_41: 1.1, county_ohe_42: 1.1, county_ohe_43: 1.1, county_ohe_44: 1.1, county_ohe_45: 1.1, county_ohe_46: 1.1, county_ohe_47: 1.1, county_ohe_48: 1.1, county_ohe_49: 1.1, county_ohe_50: 1.1, county_ohe_51: 1.1, county_ohe_52: 1.1, county_ohe_53: 1.1, county_ohe_54: 1.1, county_ohe_55: 1.1, county_ohe_56: 1.1, county_ohe_57: 1.1, county_ohe_58: 1.1, county_ohe_59: 1.1, county_ohe_60: 1.1, county_ohe_61: 1.1, county_ohe_62: 1.1, county_ohe_63: 1.1, county_ohe_64: 1.1, county_ohe_65: 1.1, county_ohe_66: 1.1, line_ohe_0: 1.1, line_ohe_1: 1.1, construction_ohe_0: 1.1, construction_ohe_1: 1.1, construction_ohe_2: 1.1, construction_ohe_3: 1.1)
-            else{
-                fatalError("Unexpected runtime error.")
-            }
-        
-        UserDefaults.standard.set(Double(modelOutput.classProbability[0]!), forKey: "strokeScore")
-    }
-    
-    
 }
